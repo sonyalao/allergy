18234f8915b884a46adeaaecc99dfe89efe5343a
Austin Chan <austin.ot.chan@gmail.com>
2019-10-12 17:17:17 -0700

    sign in page example

---
+++ my-app/src/SignIn/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SignIn extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>INSIDE SIGN IN</p>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
